2e6877b86545766228ce9c901c5e00ad576d1afe
Mir Imad Ahmed <mirimadahmed@outlook.com>
2022-11-03 01:29:14 +0500

    update with axios

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,48 @@
 <template>
   <div id="app">
-    <div class="row">
-      <div class="column">
-        <input
-          type="email"
-          placeholder="Email:"
-          v-model="email"
-          class="input"
-        />
+    <div class="wrapper">
+      <div class="row" v-if="error">
+        <div class="column">
+          {{ error }}
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="column">
-        <input type="text" placeholder="Name:" v-model="name" class="input" />
+      <div class="row">
+        <div class="column">
+          <input
+            type="email"
+            placeholder="Email:"
+            v-model="email"
+            class="input"
+          />
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="column">
-        <vue-recaptcha
-          style="width: none; height: none"
-          :sitekey="siteKey"
-          @verify="verifyMethod"
-          class="recaptcha"
-        >
-        </vue-recaptcha>
+      <div class="row">
+        <div class="column">
+          <input type="text" placeholder="Name:" v-model="name" class="input" />
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="column">
-        <button>Submit</button>
+      <div class="row">
+        <div class="column">
+          <vue-recaptcha
+            style="width: none; height: none"
+            :sitekey="siteKey"
+            @verify="verifyMethod"
+            class="recaptcha"
+          >
+          </vue-recaptcha>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column">
+          <button :disabled="!verified" @click="submitReg">Submit</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import { VueRecaptcha } from "vue-recaptcha";
 export default {
   name: "App",
@@ -44,11 +52,30 @@ export default {
       name: "",
       email: "",
       siteKey: "6LfmB9IiAAAAAB9OLZZj83qPRgSdcTbHDwtkeJ82",
+      verified: false,
+      error: null,
     };
   },
   methods: {
     verifyMethod() {
+      this.verified = true;
       alert("verified");
+    },
+    submitReg() {
+      this.error = null;
+      axios
+        .post("http://54.174.5.102:3001/api/user", {
+          name: this.name,
+          email: this.email,
+        })
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.error) {
+            this.error = res.data.message;
+          } else {
+            this.error = "Successful Registration.";
+          }
+        });
     },
   },
 };
@@ -97,4 +124,13 @@ button {
   color: white;
   padding: 20px 40px 20px 40px;
 }
+
+.wrapper {
+  width: 80%;
+  padding-left: 40px;
+  padding-right: 40px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
 </style>
